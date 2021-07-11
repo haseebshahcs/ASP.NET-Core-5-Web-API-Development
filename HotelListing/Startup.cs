15bae8f2ac63c206511f5a373670c7070e71e8a1
Syed Muhammad Haseeb Shah <haseeb.shah@systemsltd.com>
2021-07-11 20:23:54 +0500

    Addes Hotel and Country controllers and get methods and fixed UnitOfWork DI

--- HotelListing/Startup.cs
+++ HotelListing/Startup.cs
@@ -1,5 +1,7 @@
 using HotelListing.Configurations;
 using HotelListing.Data;
+using HotelListing.IRepository;
+using HotelListing.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -43,12 +45,17 @@ namespace HotelListing
             //registering autommaper here
             services.AddAutoMapper(typeof(MapperInitializer));
 
+            //AddTransient means everytime its needed a new instand will be created
+            //AddScoped means a new instance is created for a period or lifetime of a certain set of requests
+            //AddSingleton means only 1 instance will exists for entire duration of the application
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
+
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "HotelListing", Version = "v1" });
             });
 
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson(op => op.SerializerSettings.ReferenceLoopHandling = Newtonsoft.Json.ReferenceLoopHandling.Ignore);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
